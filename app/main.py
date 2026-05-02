0c668790372754c384230da973083a1398c3a016
Mariia Madurova <marovudam@gmail.com>
2026-05-02 13:54:51 +0300

    Структура проекта

---
+++ app/main.py
@@ -0,0 +1,14 @@
+# from fastapi import FastAPI
+
+# from app.routers.tasks import router
+# from app.services.middleware import RequestContextMiddleware
+
+
+# app = FastAPI()
+
+# app.add_middleware(RequestContextMiddleware)
+# app.include_router(router)
+
+# @app.get("/")
+# def root():
+#     return "OK"
